386ec254a553c7db93e74736adfe924617dd6edd
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-01-08 18:03:25 -0700

    Complete implementation of flight booker task.

--- svelte/src/tasks/flightBooker.svelte
+++ svelte/src/tasks/flightBooker.svelte
@@ -1,24 +1,83 @@
 <script>
   import Card from "../components/card.svelte";
 
-  let flightType = 1
-  let startDate = new Date()
-  let returnDate
+  let flightMap = {
+    1: "one-way flight",
+    2: "return flight"
+  };
 
-  function formatDate() {}
+  let flightType = 1;
+  let startDate = formatAsString(new Date());
+  let returnDate = formatAsString(new Date());
+
+  $: error1 = !tryAsDate(startDate);
+  $: error2 = !tryAsDate(returnDate);
+  $: error3 = tryAsDate(returnDate) < tryAsDate(startDate);
+
+  function formatAsString(date) {
+    return date
+      .toISOString()
+      .slice(0, 10)
+      .split("-")
+      .reverse()
+      .join(".");
+  }
+
+  function tryAsDate(dateString) {
+    let dateArr = dateString.split(".").reverse();
+    if (dateArr[1].length !== 2 || dateArr[2].length !== 2) {
+      return false;
+    }
+    let date = new Date(dateArr.join("-"));
+    return date.valueOf() || false;
+  }
+
+  function book() {
+    let timeStrings = {
+      1: " for " + startDate,
+      2: " from " + startDate + " to " + returnDate
+    };
+    alert(
+      "You have booked a " + flightMap[flightType] + timeStrings[flightType]
+    );
+  }
 </script>
 
 <Card title="Flight Booker">
-  <select bind:value={flightType}>
-    <option value={1}>one-way flight</option>
-    <option value={2}>return flight</option>
-  </select>
-  <input />
-  <input disabled={flightType === 1}/>
+  <div>
+    <select bind:value={flightType}>
+      <option value={1}>{flightMap[1]}</option>
+      <option value={2}>{flightMap[2]}</option>
+    </select>
+    <input bind:value={startDate} class:error={error1} />
+    <input
+      bind:value={returnDate}
+      class:error={error2}
+      disabled={flightType === 1} />
+    <button on:click={book} disabled={error1 || error2 || error3}>Book</button>
+  </div>
 </Card>
 
 <style>
-  input:disabled {
-    background: #ddd;
+  div {
+    max-width: 300px;
+    margin: auto;
+  }
+
+  div > * {
+    width: 100%;
+    display: block;
+  }
+
+  button {
+    background: #fff;
+  }
+
+  :disabled {
+    background: #eee;
+  }
+
+  .error {
+    background: #dd9999;
   }
 </style>
