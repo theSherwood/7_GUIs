3b9cf7323a82fb23788e0a11ee000f9c8f80d045
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-01-13 12:37:08 -0700

    React-vanilla: initial commit.

---
+++ react-vanilla/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
