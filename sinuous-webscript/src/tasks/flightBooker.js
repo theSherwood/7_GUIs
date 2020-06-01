b54f75c97ed4a77361927c7f96a9512443dcb689
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-06-01 17:01:55 -0600

    sinuous-webscript: translate flightBooker.

--- sinuous-webscript/src/tasks/flightBooker.js
+++ sinuous-webscript/src/tasks/flightBooker.js
@@ -1,11 +1,12 @@
-import { html, o } from "sinuous";
+import { o } from "sinuous";
 import { S } from "sinuous/observable";
+import { div, select, option, input, button } from "../elements";
 import { card } from "../components/card";
 import "./flightBooker.css";
 
 let flightMap = {
   1: "one-way flight",
-  2: "return flight"
+  2: "return flight",
 };
 
 function tryAsDate(dateString) {
@@ -18,12 +19,7 @@ function tryAsDate(dateString) {
 }
 
 function formatAsString(date) {
-  return date
-    .toISOString()
-    .slice(0, 10)
-    .split("-")
-    .reverse()
-    .join(".");
+  return date.toISOString().slice(0, 10).split("-").reverse().join(".");
 }
 
 export const flightBooker = () => {
@@ -43,35 +39,30 @@ export const flightBooker = () => {
   function book() {
     let timeStrings = {
       1: " for " + startDate(),
-      2: " from " + startDate() + " to " + returnDate()
+      2: " from " + startDate() + " to " + returnDate(),
     };
     alert(
       "You have booked a " + flightMap[flightType()] + timeStrings[flightType()]
     );
   }
 
-  return html`
-    <${card} title="Flight Booker">
-      <div>
-        <select value=${flightType} onchange=${e => flightType(e.target.value)}>
-          <option value="{1}">${flightMap[1]}</option>
-          <option value="{2}">${flightMap[2]}</option>
-        </select>
-        <input
-          value=${startDate}
-          class=${startDateClass}
-          oninput=${e => startDate(e.target.value)}
-        />
-        <input
-          value=${returnDate}
-          class=${returnDateClass}
-          oninput=${e => returnDate(e.target.value)}
-          disabled=${isOneWay}
-        />
-        <button onclick=${book} disabled=${isBookButtonDisabled}>
-          Book
-        </button>
-      </div>
-    <//>
-  `;
+  return card(
+    { title: "Flight Booker" },
+    div(
+      select.value(flightType).onchange((e) => flightType(e.target.value))(
+        option.value`1`(flightMap[1]),
+        option.value`2`(flightMap[2])
+      ),
+      input
+        .value(startDate)
+        .class(startDateClass)
+        .oninput((e) => startDate(e.target.value)),
+      input
+        .value(returnDate)
+        .class(returnDateClass)
+        .oninput((e) => returnDate(e.target.value))
+        .disabled(isOneWay),
+      button.onclick(book).disabled(isBookButtonDisabled)("Book")
+    )
+  );
 };
