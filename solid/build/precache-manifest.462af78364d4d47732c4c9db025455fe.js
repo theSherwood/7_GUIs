a2468fb35cf1c950d5386a53ffe16fcc3f1cf6de
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-06-04 03:59:26 -0600

    Fix build paths and push builds for svelte and solid.

---
+++ solid/build/precache-manifest.462af78364d4d47732c4c9db025455fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4412525fb43debba2051055d07da518",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6384134d53370bd13e38",
+    "url": "./static/css/main.0b47d636.chunk.css"
+  },
+  {
+    "revision": "09eba05aea87b4fb08ac",
+    "url": "./static/js/2.373b6c6d.chunk.js"
+  },
+  {
+    "revision": "6384134d53370bd13e38",
+    "url": "./static/js/main.068904a9.chunk.js"
+  },
+  {
+    "revision": "2c0f1c0a0c6e803ce2a2",
+    "url": "./static/js/runtime~main.8a79a2da.js"
+  }
+]);
