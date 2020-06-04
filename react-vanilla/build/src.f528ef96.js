05d4f708f840fa0fd7af15116d1ca59c9df32813
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-06-04 03:43:54 -0600

    react-vanilla: another build.

--- react-vanilla/build/src.f528ef96.js
+++ react-vanilla/build/src.f528ef96.js
@@ -25,4 +25,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"react":"HdMw","./tasks/Counter":"XIFW","./tasks/TemperatureConverter":"C8Zv","./App.css":"TRXA"}],"H99C":[function(require,module,exports) {
 "use strict";var e=u(require("react")),r=u(require("react-dom")),t=u(require("./App"));function u(e){return e&&e.__esModule?e:{default:e}}r.default.render(e.default.createElement(t.default,null),document.getElementById("root"));
 },{"react":"HdMw","react-dom":"X9zx","./App":"FLHj"}]},{},["H99C"], null)
-//# sourceMappingURL=/src.f528ef96.js.map
+//# sourceMappingURL=react-vanilla/build/src.f528ef96.js.map
