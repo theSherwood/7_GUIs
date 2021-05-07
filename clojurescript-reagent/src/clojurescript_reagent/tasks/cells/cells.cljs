288ea2cb974e64da1c7d9c0888976a097ee42ea2
Adam Sherwood <adam.the.sherwood@gmail.com>
2021-05-07 10:24:48 -0600

    refactor: for resusability

--- clojurescript-reagent/src/clojurescript_reagent/tasks/cells/cells.cljs
+++ clojurescript-reagent/src/clojurescript_reagent/tasks/cells/cells.cljs
@@ -29,19 +29,6 @@
       (= direction :after) (nth vec (inc idx) nil)
       :else nil)))
 
-(def shape [100 100])
-
-(def rows (mapv str (range (first shape))))
-(def columns (vec (letter-range (second shape))))
-
-(def *cell-data (r/atom (into {}
-                              (map
-                               (fn [[k v]]
-                                 [k (parse-string v rows columns)])
-                               sample-data))))
-
-(def *cell-cache (atom {}))
-
 (declare resolve-cells)
 
 (defn compute-cell-value [cell cell-data *cell-cache]
@@ -69,46 +56,14 @@
      []
      cell-ids)))
 
-(def t-body (atom nil))
-
-(defn handle-change [e *cell]
-  (reset! *cell-cache {})
-  (reset! *cell (parse-string (.. e -target -value) rows columns)))
-
-(defn handle-key-down [e c r]
-  (let [selector
-        (cond
-          (= (.-key e) "ArrowUp")
-          (let [new-row (find-adjacent rows r :before)]
-            (if-not (nil? new-row) (str c new-row)))
-
-          (not= -1 (.indexOf ["ArrowDown" "Enter"] (.-key e)))
-          (let [new-row (find-adjacent rows r :after)]
-            (if-not (nil? new-row) (str c new-row)))
-
-          (and (= (.-key e) "ArrowLeft") (.-altKey e))
-          (let [new-column (find-adjacent columns c :before)]
-            (if-not (nil? new-column) (str new-column r)))
-
-          (and (= (.-key e) "ArrowRight") (.-altKey e))
-          (let [new-column (find-adjacent columns c :after)]
-            (if-not (nil? new-column) (str new-column r))))]
-    (if selector
-      (do
-        (.preventDefault e)
-        (.focus (.querySelector @t-body (str "#cell-" selector)))))))
-
-(defn clear []
-  (reset! *cell-data {}))
-
-(defn head []
+(defn head [columns]
   [:thead
    (into
     [:tr [:td {:class "row-key"}]]
     (map (fn [column] [:td {:class "column-key"} column])
          columns))])
 
-(defn input [{:keys [c r *focused *cell]}]
+(defn input [{:keys [c r *focused *cell handle-change handle-key-down]}]
   (let [cell @*cell]
     [:input {:id (str "cell-" c r)
              :auto-focus true
@@ -118,12 +73,14 @@
              :on-key-down #(handle-key-down % c r)
              :on-blur #(reset! *focused false)}]))
 
-(defn computed-string [cell]
+(defn computed-string [cell *cell-data *cell-cache]
   ;; We put this in its own fn so that we get the reactive barrier between this
   ;; computation and the rest of the component
   (str (compute-cell-value cell @*cell-data *cell-cache)))
 
-(defn cell-view [c r]
+(defn cell-view
+  [{:as props
+    :keys [c r *cell-data *cell-cache *t-body]}]
   (r/with-let
     [cell-id  (str c r)
      input-id (str "cell-" c r)
@@ -133,7 +90,7 @@
                     (if (and (not o) n)
                       (js/setTimeout
                        (fn []
-                         (let [target-input (.querySelector @t-body 
+                         (let [target-input (.querySelector @*t-body
                                                             (str "#" input-id))]
                            (.. target-input (setSelectionRange 0 99999))))
                        10))))
@@ -143,7 +100,9 @@
       [:td {:id cell-id
             :on-click #(reset! *focused true)}
        (if @*focused
-         [input {:c c :r r :*cell *cell :*focused *focused}]
+         [input (merge props
+                       {:*cell *cell
+                        :*focused *focused})]
          [:div {:id input-id
                 :tabIndex 0
                 :on-focus #(reset! *focused true)}
@@ -151,23 +110,70 @@
            ;; Just render the string from the cell
             (:string cell)
            ;; Compute the string to display from *cell-data and *cell-cache
-            [computed-string cell])])])))
+            [computed-string cell *cell-data *cell-cache])])])))
 
 (defn main []
-  [card/main
-   "Cells"
-   [:<>
-    [:div {:class "wrapper"}
-     [:table
-      [head]
-      [:tbody {:ref #(reset! t-body %)}
-       (doall
-        (for [r rows]
-          ^{:key r}
-          [:tr {:id (str "row-" r)}
-           [:td {:class "row-key"} r]
-           (doall
-            (for [c columns]
-              ^{:key (str c r)}
-              [cell-view c r]))]))]]]
-    [:button {:on-click clear} "Clear"]]])
+  (r/with-let
+    [shape [100 100]
+     rows (mapv str (range (first shape)))
+     columns (vec (letter-range (second shape)))
+
+     *cell-data (r/atom (into {}
+                              (map
+                               (fn [[k v]]
+                                 [k (parse-string v rows columns)])
+                               sample-data)))
+     *cell-cache (atom {})
+
+     *t-body (atom nil)
+
+     handle-change (fn handle-change [e *cell]
+                     (reset! *cell-cache {})
+                     (reset! *cell (parse-string (.. e -target -value) rows columns)))
+     handle-key-down (fn handle-key-down [e c r]
+                       (let [selector
+                             (cond
+                               (= (.-key e) "ArrowUp")
+                               (let [new-row (find-adjacent rows r :before)]
+                                 (if-not (nil? new-row) (str c new-row)))
+
+                               (not= -1 (.indexOf ["ArrowDown" "Enter"] (.-key e)))
+                               (let [new-row (find-adjacent rows r :after)]
+                                 (if-not (nil? new-row) (str c new-row)))
+
+                               (and (= (.-key e) "ArrowLeft") (.-altKey e))
+                               (let [new-column (find-adjacent columns c :before)]
+                                 (if-not (nil? new-column) (str new-column r)))
+
+                               (and (= (.-key e) "ArrowRight") (.-altKey e))
+                               (let [new-column (find-adjacent columns c :after)]
+                                 (if-not (nil? new-column) (str new-column r))))]
+                         (if selector
+                           (do
+                             (.preventDefault e)
+                             (.focus (.querySelector @*t-body (str "#cell-" selector)))))))
+     clear (fn clear []
+             (reset! *cell-data {}))]
+    [card/main
+     "Cells"
+     [:<>
+      [:div {:class "wrapper"}
+       [:table
+        [head columns]
+        [:tbody {:ref #(reset! *t-body %)}
+         (doall
+          (for [r rows]
+            ^{:key r}
+            [:tr {:id (str "row-" r)}
+             [:td {:class "row-key"} r]
+             (doall
+              (for [c columns]
+                ^{:key (str c r)}
+                [cell-view {:c c
+                            :r r
+                            :handle-change   handle-change
+                            :handle-key-down handle-key-down
+                            :*cell-data *cell-data
+                            :*cell-cache *cell-cache
+                            :*t-body *t-body}]))]))]]]
+      [:button {:on-click clear} "Clear"]]]))
